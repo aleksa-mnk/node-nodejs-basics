8d2f52ba6f601cc821c661a157cbf7a95bdecfb4
aleksa-mnk <aleksamnk@outlook.com>
2024-01-29 22:40:23 +0300

    feat: read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
-const read = async () => {
-    // Write your code here 
-};
+import { promises as fs } from 'fs'
+import { join } from 'path'
 
-await read();
+const read = async () => {
+    try {
+        const filePath = join('src', 'fs', 'files', 'fileToRead.txt')
+
+        await fs.access(filePath)
+
+        const content = await fs.readFile(filePath, 'utf8')
+        console.log(content)
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
+}
+
+await read()
