1cd2904a1c9ac7a9308801826ecc3805d3abeda6
aleksa-mnk <aleksamnk@outlook.com>
2024-01-30 00:07:25 +0300

    feat: worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from 'worker_threads'
+import os from 'os'
 
-await performCalculations();
+const performCalculations = async () => {
+    const numCores = os.cpus().length
+    let workers = []
+    let results = []
+
+    for (let i = 0; i < numCores; i++) {
+        const worker = new Worker('./src/wt/worker.js')
+        worker.postMessage(10 + i)
+
+        workers.push(new Promise((resolve, reject) => {
+            worker.on('message', (result) => resolve({ status: 'resolved', data: result }))
+            worker.on('error', (error) => resolve({ status: 'error', data: null }))
+            worker.on('exit', (code) => {
+                if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`))
+            })
+        }))
+    }
+
+    results = await Promise.all(workers)
+    console.log(results)
+}
+
+await performCalculations()
