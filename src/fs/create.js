ba9e9f29aece9eb61e57ea8c21e75ad5fe11f2f8
aleksa-mnk <aleksamnk@outlook.com>
2024-01-29 22:08:38 +0300

    feat: create fresh.txt

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
-const create = async () => {
-    // Write your code here 
-};
+import { promises as fs } from 'fs'
+import { join } from 'path'
 
-await create();
+const create = async () => {
+    try {
+        const filePath = join('src', 'fs', 'files', 'fresh.txt')
+        try {
+            await fs.access(filePath)
+            throw new Error('FS operation failed')
+        } catch (error) {
+            await fs.writeFile(filePath, 'I am fresh and young')
+        }
+    } catch (error) {
+        throw error
+    }
+}
+
+create().catch(err => console.error(err))
