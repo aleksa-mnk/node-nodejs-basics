c73c00dfe8fc4f07f86cbf79e2a4e729b43c4d1b
aleksa-mnk <aleksamnk@outlook.com>
2024-01-29 22:12:50 +0300

    feat: copy files folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { promises as fs } from 'fs'
+import { join } from 'path'
+import { cp } from 'fs/promises'
 
-await copy();
+const copy = async () => {
+    try {
+        const sourceFolder = join('src', 'fs', 'files')
+        const targetFolder = join('src', 'fs', 'files_copy')
+
+        const sourceExists = await fs.access(sourceFolder).then(() => true).catch(() => false)
+        const targetExists = await fs.access(targetFolder).then(() => true).catch(() => false)
+
+        if (!sourceExists || targetExists) {
+            throw new Error('FS operation failed')
+        }
+
+        await cp(sourceFolder, targetFolder, { recursive: true })
+    } catch (error) {
+        throw error
+    }
+}
+
+await copy()
