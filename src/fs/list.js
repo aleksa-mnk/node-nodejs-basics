285e60ff66b417a98fddefafe848dbb3d4a74de0
aleksa-mnk <aleksamnk@outlook.com>
2024-01-29 22:27:07 +0300

    feat: list of files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
-const list = async () => {
-    // Write your code here 
-};
+import { promises as fs } from 'fs'
+import { join } from 'path'
 
-await list();
+const list = async () => {
+    try {
+        const folderPath = join('src', 'fs', 'files')
+
+        await fs.access(folderPath)
+
+        const files = await fs.readdir(folderPath)
+        console.log(files)
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
+}
+
+await list()
