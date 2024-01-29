53b2cf01d67ffa4c53db9a98598b20665fd618de
aleksa-mnk <aleksamnk@outlook.com>
2024-01-29 22:18:06 +0300

    feat: rename ro properFilename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
-const rename = async () => {
-    // Write your code here 
-};
+import { promises as fs } from 'fs'
+import { join } from 'path'
 
-await rename();
+const rename = async () => {
+    try {
+        const oldPath = join('src', 'fs', 'files', 'wrongFilename.txt')
+        const newPath = join('src', 'fs', 'files', 'properFilename.md')
+
+        const oldExists = await fs.access(oldPath).then(() => true).catch(() => false)
+        const newExists = await fs.access(newPath).then(() => true).catch(() => false)
+
+        if (!oldExists || newExists) {
+            throw new Error('FS operation failed')
+        }
+
+        await fs.rename(oldPath, newPath)
+    } catch (error) {
+        throw error
+    }
+}
+
+await rename()
